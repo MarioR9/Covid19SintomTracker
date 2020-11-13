bd15ebaa94f4d1a3e25f1ba6aee2196f9e7f8c23
Mario Rodriguez <31521338+MarioR9@users.noreply.github.com>
2020-11-12 22:42:16 -0500

    Update Main.jsx

--- Main.jsx
+++ Main.jsx
@@ -5,8 +5,11 @@ import React from 'react';
 const Main = () => {
 	return (
 	  <div>
-      <h1>HOME</h1>
-		</div>
+     	 	<h1>HOME</h1>
+			<div>
+			<h1>Table</h1>
+			</div>
+	 </div>
 	);
 };
 
